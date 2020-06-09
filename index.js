aa24e4b74014f34149af8e3d534e5dbe5fc8d27c
imane <benbenimane@hotmail.com>
2020-06-09 21:20:09 +0200

    created a rest api,  first  route in express yay

--- index.js
+++ index.js
@@ -2,13 +2,35 @@ const express  = require('express');
 const  path = require('path');
 const app = express();
 
+//route
+
+  const members  =  [
+    {
+      id: 1,
+      name: 'sharon',
+      email: 'sharon@hey.com',
+      status: 'active'
+    },
+    {
+      id: 1,
+      name: 'bob',
+      email: 'bob@hey.com',
+      status: 'inactive'
+    },
+    {
+      id: 1,
+      name: 'wim',
+      email: 'wim@hey.com',
+      status: 'active'
+    }
+  ];
+
+  //this route gets all members
+  app.get('/api/members',(req,res)=>res.json(members));
+
+
+
 //set a static folder
 app.use(express.static(path.join(__dirname,'public')));
-
-app.get('/',  (req,res)=>{
-res.sendFile(path.join(__dirname,'public',  'index.html'));
-});
-
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT, ()=> console.log(`server running on ${PORT}`))
